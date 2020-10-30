d55b38d5fb43deba617b920964600e1586542e6a
Alejandro Clavera <alejandroclavera@Macbook-Pro-de-Alejandro.local>
2020-10-30 15:11:18 +0100

    Reto collaz

---
+++ Collatz/Collatz.py
@@ -0,0 +1,19 @@
+result={'number':None,'max_length':0}
+for n in range(1,100001):
+    result[n] = 1
+    number = n
+    while number != 1:
+        if n != number and number in result:
+            result[n]+= result[number]    
+            break
+        number = number/2 if number%2==0 else number * 3 + 1
+        result[n] += 1 
+    if result['max_length'] < result[n]:
+                result['max_length'] = result[n]
+                result['number'] = n 
+print(result['number'])
+
+
+
+    
+
